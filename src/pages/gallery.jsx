fd825153996b6a9a4dbd96054a60330a042f207a
Aayush Shukla <oreus.aks@gmail.com>
2024-05-27 23:48:48 +0530

    Trying to add images, not appearing on page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -9,6 +9,7 @@ const gallery = () => {
         <Header />
         <div className='bg-slate-950 p-9'>
           <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
+          <img src='./dummy.jpg'/>
         </div>
         <Footer />
     </>
