203a925f09cb4e2221c228cff0e14ab24fbad844
Aayush Shukla <oreus.aks@gmail.com>
2024-05-27 23:36:29 +0530

    Keeping a consistent design layout, text not appearing in page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/header'
 import './Gallery.css'
-import TitleText from '../components/titleText'
+import Footer from '../components/footer'
 
 const gallery = () => {
   return (
     <>
         <Header />
-        <TitleText />
+        <div className='bg-slate-950 p-9'>
+          <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
+        </div>
+        <Footer />
     </>
   )
 }
