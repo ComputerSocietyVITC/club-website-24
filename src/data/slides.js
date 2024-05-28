bd09f16e0cd6446565340740e044ebf9bc61305f
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 10:28:19 +0530

    Changed the dimensions same as in the figma file

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,28 +1,57 @@
 export const slides = [
     {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
+        width: 534,
+        height: 420.97
     },
     {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
-    },{
+        width: 534,
+        height: 420.97
+    },
+    {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
-    },{
+        width: 534,
+        height: 420.97
+    },
+    {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
-    },{
+        width: 534,
+        height: 420.97
+    },
+    {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
-    },{
+        width: 534,
+        height: 420.97
+    },
+    {
         src: './dummy.jpg',
-        width: 800,
-        height: 600
+        width: 534,
+        height: 420.97
+    },
+    {
+        src: './dummy.jpg',
+        width: 534,
+        height: 420.97
+    },
+    {
+        src: './dummy.jpg',
+        width: 534,
+        height: 420.97
+    },
+    {
+        src: './dummy.jpg',
+        width: 534,
+        height: 420.97
+    },
+    {
+        src: './dummy.jpg',
+        width: 534,
+        height: 420.97
+    },
+    {
+        src: './dummy.jpg',
+        width: 534,
+        height: 420.97
     },
 ]
