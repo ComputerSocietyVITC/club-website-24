b596e614b2ba6988a8b6bfe49fcc8158448c2e18
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 13:19:26 +0530

    Change layout of gallery from rows to masonry

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -7,7 +7,7 @@ const Gallery = () => {
     <div className='bg-slate-950 p-9'>
       <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
       <PhotoAlbum
-        layout="rows"
+        layout="masonry"
         photos={slides}
         columns={3}
       />
