2cad8b03cfe4c77ddaa383a4982a1bdd010385e4
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 20:29:04 +0530

    Very basic layout done

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='bg-slate-950 p-9'>
+      <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Projects<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
+    </div>
   )
 }
 
