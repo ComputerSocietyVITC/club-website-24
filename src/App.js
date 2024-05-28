fec86df61131f75e8d0f78f1026be5aae2a70316
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 09:56:25 +0530

    Set up the routes for all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/footer';
 import Homepage from './pages/homepage';
 import Events from './pages/events';
 import Gallery from './pages/gallery';
+import Projects from './pages/projects';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Homepage />}/>
             <Route path="/events" element={<Events />}/>
             <Route path="/gallery" element={<Gallery />}/>
+            <Route path="/projects" element={<Projects />}/>
           </Routes>
         <Footer />
       </BrowserRouter>
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
     <>
         <div className='bg-slate-950 p-9'>
           <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
-          <img src='./dummy.jpg'/>
+          <img src='./dummy.jpg' alt="test"/>
         </div>
     </>
   )
