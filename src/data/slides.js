2a9fab5338f95ebe6ff86a14699ea6cd22fd4e2d
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 10:24:10 +0530

    Push some dummy images and create a dummy album

---
+++ src/data/slides.js
@@ -0,0 +1,28 @@
+export const slides = [
+    {
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },
+    {
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },{
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },{
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },{
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },{
+        src: './dummy.jpg',
+        width: 800,
+        height: 600
+    },
+]
