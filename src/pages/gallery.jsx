6c38984fbdd637fee5d751a99cbda5bd5112d58b
Aayush Shukla <oreus.aks@gmail.com>
2024-05-28 10:47:13 +0530

    Removed react fragment from gallery page and made div the parent element

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -4,16 +4,14 @@ import { slides } from '../data/slides'
 
 const Gallery = () => {
   return (
-    <>
-        <div className='bg-slate-950 p-9'>
-          <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
-          <PhotoAlbum
-            layout="rows"
-            photos={slides}
-            columns={3}
-          />
-        </div>
-    </>
+    <div className='bg-slate-950 p-9'>
+      <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Gallery<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
+      <PhotoAlbum
+        layout="rows"
+        photos={slides}
+        columns={3}
+      />
+    </div>
   )
 }
 
