9ae2ebb68015eb55a6a21a2aa264527c88a0ef5a
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2024-05-29 17:35:25 +0000

    routes configured

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import Projects from './pages/projects';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const basename = process.env.PUBLIC_URL || ''; // This will help in local and production environments
+
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Header />
-          <Routes>
-            <Route path="/" element={<Homepage />}/>
-            <Route path="/events" element={<Events />}/>
-            <Route path="/gallery" element={<Gallery />}/>
-            <Route path="/projects" element={<Projects />}/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
