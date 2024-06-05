d0500f7fbb0b3b4e445757e5f6dde776c1a1fa13
Aayush Shukla <oreus.aks@gmail.com>
2024-06-05 10:42:43 +0530

    Made basic Events page & using universal CSS

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,42 +1,33 @@
 import React from 'react'
 import Header from '../components/header'
-import './events.css'
 
 const events = () => {
+    
+
 return (
     <>
     <Header type={0} />
-    <div className='bg-slate-950 p-9 root'>
-        <h1 className='text-white text-9xl font-bold text-center pb-16'> <span className='text-emerald-500 font-extrabold'>&lt;/</span>Events<span className='text-emerald-500 font-extrabold'>&gt;</span></h1>
-        <div className='flex flex-col  items-center group w-full h-full gap-y-[230px] mb-32'>
-            <div className='relative h-[400px]'>
-                <div className='w-[1100px] h-[400px] border-white border-4 p-16 rounded-2xl z-10'><h1 className='text-white text-center text-3xl font-bold align-center'>Insert Photo</h1></div>
-                <div className=' absolute bottom-[-30%] w-[1100px] h-[135px] border-emerald-500 border-4 border-t-0 border-dashed rounded-b-2xl z-0 px-[5%]'>
-                    <div className='absolute bottom-[15%] w-[80%] text-white flex flex-col gap-y-3'>
-                        <h2 className='text-4xl font-semibold text-emerald-500'>Event name</h2>
-                        <div className='flex justify-between'>
-                            <h3 className='text-2xl font-mono'>Date:XX/XX/XXXX</h3>
-                            <h3 className='text-2xl font-mono'>Venue:</h3>
-                            <h3 className='text-2xl font-mono'>Prize Pool:XXXX/-</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='relative h-[400px]'>
-                <div className='w-[1100px] h-[400px] border-white border-4 p-16 rounded-2xl z-10'><h1 className='text-white text-center text-3xl font-bold align-center'>Insert Photo</h1></div>
-                <div className=' absolute bottom-[-30%] w-[1100px] h-[135px] border-emerald-500 border-4 border-t-0 border-dashed rounded-b-2xl z-0 px-[5%]'>
-                    <div className='absolute bottom-[15%] w-[80%] text-white flex flex-col gap-y-3'>
-                        <h2 className='text-4xl font-semibold text-emerald-500'>Event name</h2>
-                        <div className='flex justify-between'>
-                            <h3 className='text-2xl font-mono'>Date:XX/XX/XXXX</h3>
-                            <h3 className='text-2xl font-mono'>Venue:</h3>
-                            <h3 className='text-2xl font-mono'>Prize Pool:XXXX/-</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="flex min-h-screen items-center gradient-background">
+    <div class="relative overflow-hidden">
+        <div class="absolute -inset-x-1 top-[-25.5%] z-10 h-1/2 scale-x-125 scale-y-75 2xl:scale-y-95"></div>
+        <div class="absolute -inset-x-1 bottom-[-25.5%] z-10 h-1/2 scale-x-125 scale-y-75 2xl:scale-y-95"></div>
+
+        <div class="marquee-10 sm:marquee-15 xl:marquee-30 w-full">
+        <div class="grid h-96 w-[350vw] grid-cols-12 gap-1 sm:h-[40rem] sm:w-[300vw] lg:w-[200vw] 2xl:h-[60rem]">
+            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-one.webp" alt="" />
+            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-six.webp" alt="" />
+            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-two.webp" alt="" />
+            <img class="h-full w-full object-cover object-top" src="https://tlfolio.netlify.app/images/me/show-four.webp" alt="" />
+            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-seven.webp" alt="" />
+            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-five.webp" alt="" />
+            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-six.webp" alt="" />
+            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-nine.webp" alt="" />
+            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-thirteen.webp" alt="" />
         </div>
-    </div></>
+        </div>
+    </div>
+    </div>
+    </>
 )
 }
 
