e563a6cd296d15ab58b8dda93e0f241e7046660c
Aayush Shukla <oreus.aks@gmail.com>
2024-06-05 17:50:24 +0530

    Working on events page, hard to replicate the design

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,34 +1,61 @@
 import React from 'react'
-import Header from '../components/header'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const events = () => {
-    
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-return (
-    <>
-    <Header type={0} />
-    <div class="flex min-h-screen items-center gradient-background">
-    <div class="relative overflow-hidden">
-        <div class="absolute -inset-x-1 top-[-25.5%] z-10 h-1/2 scale-x-125 scale-y-75 2xl:scale-y-95"></div>
-        <div class="absolute -inset-x-1 bottom-[-25.5%] z-10 h-1/2 scale-x-125 scale-y-75 2xl:scale-y-95"></div>
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import Header from "../components/header";
 
-        <div class="marquee-10 sm:marquee-15 xl:marquee-30 w-full">
-        <div class="grid h-96 w-[350vw] grid-cols-12 gap-1 sm:h-[40rem] sm:w-[300vw] lg:w-[200vw] 2xl:h-[60rem]">
-            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-one.webp" alt="" />
-            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-six.webp" alt="" />
-            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-two.webp" alt="" />
-            <img class="h-full w-full object-cover object-top" src="https://tlfolio.netlify.app/images/me/show-four.webp" alt="" />
-            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-seven.webp" alt="" />
-            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-five.webp" alt="" />
-            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-six.webp" alt="" />
-            <img class="h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-nine.webp" alt="" />
-            <img class="col-span-2 h-full w-full object-cover" src="https://tlfolio.netlify.app/images/me/show-thirteen.webp" alt="" />
-        </div>
-        </div>
-    </div>
-    </div>
-    </>
-)
+const Events = () => {
+    return (
+        <>
+            <Header type={0} />
+            <div className="min-h-screen bg-slate-950 events-container">
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={false}
+                    centeredSlides={true}
+                    loop={true}
+                    slidesPerView={5}
+                    coverflowEffect={
+                        {
+                            rotate: 0,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 2.5
+                        }
+                    }
+                    className="swiper-container"
+                >
+                    <SwiperSlide>
+                        <img/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img/>
+                    </SwiperSlide>
+                </Swiper>
+                <div className="slider-container">
+                    
+                </div>
+                <button
+                    className='border-2 border-solid border-teal-500 p-3 rounded-full bg-gradient-to-r from-b_col1 to-b_col2'>
+                    <p className='text-teal-500 text-montserrat font-light text-lg'>View Gallery ></p>
+                </button>
+            </div>
+        </>
+    )
 }
 
-export default events
+export default Events
