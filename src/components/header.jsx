8c71e9e13054483f0fd392ae0eaf80fd23b65b6f
atPavithran <pavithranmurugavel@gmail.com>
2024-06-05 20:05:13 +0530

    error fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,13 +3,9 @@ import logo from '../ICON.png'; // Adjust the path to your image
 import { NavLink } from 'react-router-dom';
 const Header = ({ type }) => {
   return (
-<<<<<<< HEAD
     <header className={`w-full bg-gradient-to-b from-black to-slate-950 text-white p-4 shadow-md `}>
       <div className="px-10 py-4 px-7 flex justify-between items-center relative">
-=======
-    <header className={`w-full bg-gradient-to-b from-black to-slate-950 text-white p-4 shadow-md ${type ? "fixed top-0 left-0 z-50" : ""}`}>
-      <div className="py-4 px-7 flex justify-between items-center relative">
->>>>>>> eefaffcb28f50e995fea914e8fc3b915fb4b81be
+
         <img src={logo} alt="IEEE Logo" className=" absolute h-12 w-12" />
         <nav className="flex-1">
           <ul className="flex justify-end space-x-16 text-slate-500 my-2 font-bold font-montserrat text-xl ">
