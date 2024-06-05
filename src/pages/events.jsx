c5739d0fba634d075f58091c88f1aa9ffc9de5f0
Aayush Shukla <oreus.aks@gmail.com>
2024-06-05 19:19:17 +0530

    On the way to Events page: Created Title and Photo Slider

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -9,48 +9,55 @@ import 'swiper/css/navigation';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 import Header from "../components/header";
 
+import image1 from '../data/images/12.jpg';
+import Footer from "../components/footer";
+
 const Events = () => {
     return (
         <>
             <Header type={0} />
             <div className="min-h-screen bg-slate-950 events-container">
+                <div className="text-white text-9xl font-bold text-center pb-16">EVENT TITLE</div>
                 <Swiper
                     effect={"coverflow"}
                     grabCursor={false}
                     centeredSlides={true}
                     loop={true}
-                    slidesPerView={5}
+                    slidesPerView={3}
                     coverflowEffect={
                         {
                             rotate: 0,
                             stretch: 0,
                             depth: 100,
-                            modifier: 2.5
+                            modifier: 1,
+
                         }
                     }
+                    pagination={{el: '.swiper-pagination', clickable: true}}
+                    modules={[EffectCoverflow, Pagination]}
                     className="swiper-container"
                 >
                     <SwiperSlide>
-                        <img/>
+                        <img src={image1} alt={"image"}/>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img/>
+                        <img src={image1} alt={"image"}/>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img/>
+                        <img src={image1} alt={"image"}/>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img/>
+                        <img src={image1} alt={"image"}/>
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img/>
+                        <img src={image1} alt={"image"}/>
                     </SwiperSlide>
                 </Swiper>
-                <div className="slider-container">
-                    
+                <div className="slider-controler">
+                    <div className="swiper-pagination"></div>
                 </div>
                 <button
-                    className='border-2 border-solid border-teal-500 p-3 rounded-full bg-gradient-to-r from-b_col1 to-b_col2'>
+                    className='border-2 border-solid border-teal-500 p-3 rounded-full bg-gradient-to-r from-b_col1 to-b_col2 float-end mt-8'>
                     <p className='text-teal-500 text-montserrat font-light text-lg'>View Gallery ></p>
                 </button>
             </div>
