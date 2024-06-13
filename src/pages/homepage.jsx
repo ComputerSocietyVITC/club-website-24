b9b4a202b256bb6a3e6cca3b30908de1041ac75a
atPavithran <pavithranmurugavel@gmail.com>
2024-06-13 17:11:33 +0530

    Projects page done

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,8 +6,8 @@ import Footer from "../components/footer";
 const Homepage = () => {
     return (
         <>
-            <div className="min-h-screen bg-slate-950 ">
-                <div className="container mx-auto grid grid-cols-1 items-center justify-center min-h-screen ">
+            <div className="min-h-screen bg-slate-950">
+                <div className="container -mt-24 mx-auto grid grid-cols-1 items-center justify-center min-h-screen ">
                     
                         <h1 className="text-7xl font-bold leading-tight text-center text-emerald-400">
                             We are The<br />
@@ -15,7 +15,7 @@ const Homepage = () => {
                             VIT Chennai..
                         </h1>
                     
-                </div>
+                </div>  
                 <div className=' flex flex-col justify-center items-center w-[80%] m-auto gap-y-6'>
                     <p className='text-white text-montserrat font-light text-[50px] max-[1px] tracking-[-0.025em] p-8 leading-25'>DISCOVER | DEVELOP | DEPLOY</p>
                     <button className=' border-2 border-solid border-teal-500 p-4 rounded-full bg-gradient-to-r from-b_col1  to-b_col2 ' ><p className='text-teal-500 text-montserrat font-light text-2xl'>Gateway to Tech &#x25BC;</p></button>
