3a3f436ea2fef29df301caf910f568742024cde4
Aayush Shukla <oreus.aks@gmail.com>
2024-06-17 13:20:50 +0530

    Footer is not made responsive, improvised the design

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,12 +15,10 @@ const Footer = () => {
 		<div className="w-[90vw] border mx-auto"></div>
 		<div className="bg-transparent relative z-0 p-16 scale-90">
 			<footer className="font-montserrat">
-				<div className="text-gray-800 flex flex-wrap lg:flex-nowrap items-stretch">
-					
+				<div className="text-gray-800 hidden xl:flex xl:flex-nowrap items-center">			
 						<a href="https://ieeecsvitc.com" className="md:self-center flex items-center justify-center order-4 lg:order-1 md:flex-col md:flex sm:flex sm:flex-col mx-12">
 							<img src={comsocLogowhite} alt="IEEE CS VITC Logo" height={500} width={500}/>
 						</a>
-					
 					<div className="mt-3 w-full lg:w-2/12 py-6 lg:py-0 hidden lg:block order-1 lg:order-2">
 						<div className="text-lg lg:text-base text-white inline-block mb-3 custom-underline-gradient">
 							Menu
@@ -207,6 +205,134 @@ const Footer = () => {
 						</div>
 					</div>
 				</div>
+				<div className="text-gray-800 hidden lg:flex lg:flex-col items-center xl:hidden">
+					<a href="https://ieeecsvitc.com" className="flex items-center justify-center order-1 mx-12">
+						<img src={comsocLogowhite} alt="IEEE CS VITC Logo" height={500} width={500} />
+					</a>
+					<div className="flex w-full justify-around flex-wrap mt-3 order-2">
+						<div className="py-6 hidden lg:block">
+							<div className="text-lg lg:text-base text-white mb-3 custom-underline-gradient">
+								Menu
+							</div>
+							<Link to="/" className="my-3 block text-white hover:text-gray-200 text-sm">Home</Link>
+							<Link to="/team" className="my-3 block text-white hover:text-gray-200 text-sm">Team</Link>
+							<Link to="/events" className="my-3 block text-white hover:text-gray-200 text-sm">Events</Link>
+							<Link to="/projects" className="my-3 block text-white hover:text-gray-200 text-sm">Projects</Link>
+							<Link to="/gallery" className="my-3 block text-white hover:text-gray-200 text-sm">Gallery</Link>
+							<Link to="/blogs" className="my-3 block text-white hover:text-gray-200 text-sm">Blogs</Link>
+						</div>
+						<div className="mt-3 sm:w-screen md:w-screen w-full lg:w-3/12">
+							<div>
+								<div className="text-lg lg:text-base text-white mb-3 custom-underline-gradient">
+									Address
+								</div>
+								<a href="https://goo.gl/maps/C9UE9JLWnBAfQPKe7" target="_blank" rel="noreferrer" className="my-3 block text-white hover:text-gray-200 text-base lg:text-sm">
+									<section>Kelambakkam - Vandalur Rd,</section>
+									<section>Rajan Nagar, Chennai,</section>
+									<section></section>
+									<section>Tamil Nadu - 600127.</section>
+								</a>
+							</div>
+							<div>
+								<div className="text-lg lg:text-base text-white mb-3 custom-underline-gradient">
+									Email
+								</div>
+								<a href="mailto:ieeecomputersociety@vit.ac.in" target="_blank" rel="noreferrer" className="my-3 block text-white hover:text-gray-200 text-base lg:text-sm lg:text-left">
+									ieeecomputersociety@vit.ac.in
+								</a>
+							</div>
+						</div>
+						<div className="mt-3 w-full lg:w-3/12">
+							<div className="text-lg lg:text-base text-center text-white mb-3 custom-underline-gradient">
+								Social Media
+							</div>
+							<div className="grid grid-cols-4 place-items-center">
+								<div><a href="https://www.instagram.com/compsoc.vitcc/" target="_blank" rel="noreferrer"><img src={instagram} alt="Instagram" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://discord.gg/6vkY3kcZnE" target="_blank" rel="noreferrer"><img src={discord} alt="Discord" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://github.com/ComputerSocietyVITC" target="_blank" rel="noreferrer"><img src={github} alt="Github" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://www.linkedin.com/company/ieee-computer-society-vit-chennai/" target="_blank" rel="noreferrer"><img src={linkedin} alt="LinkedIn" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://www.youtube.com/channel/UCOgwDinZGau4rwv3swAe-nQ" target="_blank" rel="noreferrer"><img src={youtube} alt="youtube" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://t.me/IEEE_CS_VIT_Chennai" target="_blank" rel="noreferrer"><img src={telegram} alt="telegram" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://twitter.com/ieeecsvitc" target="_blank" rel="noreferrer"><img src={twitter} alt="twitter" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+								<div><a href="https://dev.to/ieeecsvitc" target="_blank" rel="noreferrer"><img src={devto} alt="devto" className="h-7 my-5 mx-auto lg:mx-0" /></a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="text-gray-800 block lg:hidden items-center">
+				<div className="flex justify-center">
+					<a href="https://ieeecsvitc.com" className="flex items-center justify-center mx-12">
+						<img src={comsocLogowhite} alt="IEEE CS VITC Logo" height={500} width={500} />
+					</a>
+				</div>
+				<div className="flex justify-center items-center mt-3 space-x-10">
+					<div className="text-center">
+						<div className="text-lg text-white mb-3 custom-underline-gradient">
+							Address
+						</div>
+						<a href="https://goo.gl/maps/C9UE9JLWnBAfQPKe7" target="_blank" rel="noreferrer" className="block text-white hover:text-gray-200 text-base">
+							<section>Kelambakkam - Vandalur Rd,</section>
+							<section>Rajan Nagar, Chennai,</section>
+							<section>Tamil Nadu - 600127.</section>
+						</a>
+					</div>
+					<div className="text-center">
+						<div className="text-lg text-white mb-3 custom-underline-gradient">
+							Email
+						</div>
+						<a href="mailto:ieeecomputersociety@vit.ac.in" target="_blank" rel="noreferrer" className="block text-white hover:text-gray-200 text-base">
+							ieeecomputersociety@vit.ac.in
+						</a>
+					</div>
+				</div>
+				<div className="flex flex-col items-center mt-3">
+					<div className="text-lg text-white mb-3 custom-underline-gradient">
+						Social Media
+					</div>
+					<div className="flex justify-around flex-wrap gap-4 w-full">
+						<div className="w-1/4 flex justify-center">
+							<a href="https://www.instagram.com/compsoc.vitcc/" target="_blank" rel="noreferrer">
+								<img src={instagram} alt="Instagram" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://discord.gg/6vkY3kcZnE" target="_blank" rel="noreferrer">
+								<img src={discord} alt="Discord" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://github.com/ComputerSocietyVITC" target="_blank" rel="noreferrer">
+								<img src={github} alt="Github" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://www.linkedin.com/company/ieee-computer-society-vit-chennai/" target="_blank" rel="noreferrer">
+								<img src={linkedin} alt="LinkedIn" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://www.youtube.com/channel/UCOgwDinZGau4rwv3swAe-nQ" target="_blank" rel="noreferrer">
+								<img src={youtube} alt="youtube" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://t.me/IEEE_CS_VIT_Chennai" target="_blank" rel="noreferrer">
+								<img src={telegram} alt="telegram" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://twitter.com/ieeecsvitc" target="_blank" rel="noreferrer">
+								<img src={twitter} alt="twitter" className="h-7 my-5" />
+							</a>
+						</div>
+						<div className="w-1/4 flex justify-center">
+							<a href="https://dev.to/ieeecsvitc" target="_blank" rel="noreferrer">
+								<img src={devto} alt="devto" className="h-7 my-5" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 			</footer>
 		</div></>
 	);
