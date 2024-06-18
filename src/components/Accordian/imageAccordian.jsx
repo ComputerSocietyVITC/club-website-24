64de9b6470621ef31b1c6265c34b0ee8c84fbaa6
Sam Shervin S <121488288+sam-shervin@users.noreply.github.com>
2024-06-18 02:26:35 +0000

    home page 80% perfect

--- src/components/Accordian/imageAccordian.jsx
+++ src/components/Accordian/imageAccordian.jsx
@@ -9,21 +9,18 @@ const ImageAccordian = ({ itemms, setActiveItem }) => {
     const handleToggle = (index) =>{setActive(index); setActiveItem(itemms[index]);}
     return (
     <>
-        <div className="image-accordion">
+        <div className="relative z-2 flex justify-center items-center">
             {items.map((item, index) => {
             const isActive = active === index ? "active" : "";
             return (
                 <div className='rounded-[20px] p-0.5 bg-gradient-to-b from-b_col3 to-b_col4 h-[456px] items-center text-center'>
                 <div
                 key={item.header}
-                className={`image-accordion-item  ${isActive}`}
-                onClick={() => handleToggle(index)}
+                className={`image-accordion-item ${isActive}`}
+                onMouseEnter={() => handleToggle(index)}
                 >
-                <h2 className='out_content -rotate-90'>{item.header}</h2>
-                <div className="content -rotate-90 xl:rotate-0">
-                    
-                    <h2>{item.header}</h2>
-                </div>
+                <h2 className='out_content -rotate-90 self-center'>{item.header}</h2>
+                <h2 className="content -rotate-90 xl:rotate-0">{item.header}</h2>
                 </div>
                 </div>
             );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,11 +11,11 @@ const Header = () => {
   };
 
   return (
-    <header className={`w-full text-white absolute z-10`}>
+    <header className={`w-screen text-white absolute z-50 bg-transparent`}>
       <div className="px-6 py-4 xl:px-8 xl:py-6 flex justify-between items-center relative">
         <NavLink to="/"><img src={logo} alt="IEEE Logo" className="h-12 w-12" /></NavLink>
-        <nav className="flex-1 flex justify-end xl:hidden">
-          <button onClick={handleClick} className="flex flex-col justify-center items-center p-4">
+        <nav className="flex justify-end lg:hidden xl:hidden">
+          <button onClick={handleClick} className="flex flex-col justify-center items-center py-1">
             <span className={`bg-slate-400 block transition-all duration-300 ease-out 
                             h-0.5 w-6 rounded-sm ${isOpen ? 
                             'rotate-45 translate-y-1' : '-translate-y-0.5'
@@ -33,7 +33,7 @@ const Header = () => {
             </span>
           </button>
           {isOpen && (
-            <section onClick={handleClick} className="fixed flex flex-col items-center justify-center h-screen w-screen z-40 bg-black/[0.4] backdrop-blur-lg top-[-5px]">
+            <section onClick={handleClick} className="fixed flex flex-col items-center justify-center h-screen w-screen z-40 backdrop-blur-lg top-[-5px]">
             <motion.section
               initial={{ opacity: 0 }}
               animate={{
