b81cc80c2cb97ed53075b7e2e6c94f90d8f94e74
sam-shervin <samshervin.sebastian@gmail.com>
2024-06-19 09:26:03 +0530

    unnecessary imports removed

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,11 +1,4 @@
-import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-
 const BlogCard = ({ title, date, author, imageUrl, url }) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Trigger animation only once
-    threshold: 0.3, // Trigger when 50% of the element is in view
-  });
   return (
     <a href={url}>
       <div className=" lg:hover:scale-[150%] transition duration-500 rounded-xl overflow-hidden p-4 border m-4 border-gray-700 cursor-pointer bg-gradient-to-br  from-[#371e3e] from-2% via-[#0a1e3e] to-[#040a39] to-9% grid grid-rows-2 h-full">
