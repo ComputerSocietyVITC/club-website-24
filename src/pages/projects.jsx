6934ae075805d7201e17e7dde1a4ba497969165e
Aayush Shukla <oreus.aks@gmail.com>
2024-06-26 17:15:04 +0530

    Changed the font size by a tiny bit, changed the padding values, changed the image fill behaviour

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -8,7 +8,7 @@ const Project = () => {
   return (
     <section className="min-h-screen text-white grid grid-cols-1 place-items-center">
       <div className="w-full px-6 md:px-12">
-        <div className="text-4xl md:text-7xl font-bold text-center my-16">
+        <div className="text-5xl md:text-7xl font-bold text-center mt-32 mb-8">
           Projects
         </div>
         <Swiper
@@ -20,23 +20,22 @@ const Project = () => {
         >
           {slides.map((slide, index) => (
             <SwiperSlide key={index}>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 mx-20 gap-4 items-center place-items-center">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 mx-20 gap-4 items-center place-items-center py-8">
                 <img
                   src={slide.img}
                   alt={slide.title}
-                  className="max-w-[550px] w-full max-h-[450px] object-fill md:{max-h-[346px] w-full max-w-[559px]} rounded-xl shadow-2xl shadow-[#7ac4ec]/30 sm:{max-h-[346px] max-w-[59px]}"
+                  className="max-w-[550px] w-full max-h-[450px] object-cover md:{max-h-[346px] w-full max-w-[559px]} rounded-xl shadow-2xl shadow-[#7ac4ec]/30 sm:{max-h-[346px] max-w-[59px]}"
                 />
                 <div className="rounded-[36px] p-0.5 bg-gradient-to-b flex justify-center from-b_col3 to-b_col4 h-[346px] md:h-auto lg:w-[500px] md:w-[400px] shadow-2xl shadow-[#7ac4ec]/30">
                   <div className="rounded-[calc(36px-1px)] p-6 md:p-12 md:h-auto bg-gradient-to-b md:w-full from-[#061b24] via-[#072031] to-[#000b11] flex flex-col text-center text-white max-w-[559px]">
-                    <h2 className="text-3xl md:text-4xl text-b_col3 font-semibold mb-4">
+                    <h2 className="text-[3vh] md:text-4xl text-b_col3 font-semibold mb-4">
                       {slide.title}
                     </h2>
-                      <p className="text-sm md:text-base">
-                        {slide.description}
-                      </p>
+                    <p className="text-[1.5vh] md:text-base">
+                      {slide.description}
+                    </p>
                   </div>
                 </div>
-
               </div>
             </SwiperSlide>
           ))}
