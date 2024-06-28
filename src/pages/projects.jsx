110fb13d5b9e8e6f930a8bd01ac4283a6dd3462e
Aayush Shukla <oreus.aks@gmail.com>
2024-06-28 16:16:01 +0530

    Mobile layout of projects page finalized

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -20,27 +20,26 @@ const Project = () => {
   }, []);
 
   return (
-    <div className="relative min-h-screen bg-transparent flex flex-col ml-4 items-center justify-center">
+    <div className="relative min-h-screen bg-transparent flex flex-col items-center justify-center">
       {isMobile ? (
         <div>
-          <div className="text-white text-7xl font-bold text-center pb-24">Projects</div>
+          <div className="text-white text-7xl font-bold text-center pt-32 pb-8">Projects</div>
           <div className="w-full max-w-6xl px-8">
             {slides.map((slide, index) => (
               <div key={index} className="my-16">
-                <div className="text-white text-4xl font-bold text-center">{slide.title.toUpperCase()}</div>
                 <div className="flex justify-center">
                   <img
                     src={slide.img}
                     alt={slide.title}
-                    className="w-full max-w-72 h-[400px] rounded-xl object-full my-8"
+                    className="w-auto h-auto rounded-xl my-8"
                   />
                 </div>
                 <div className="rounded-[24px] p-0.5 bg-gradient-to-b flex justify-center from-b_col3 to-b_col4 h-auto w-full md:w-auto md:h-auto shadow-2xl shadow-[#7ac4ec]/30 mt-4">
                   <div className="rounded-[calc(24px-1px)] p-6 md:p-8 bg-gradient-to-b md:w-full from-[#061b24] from-2% via-[#072031] to-[#000b11] to-9% h-full flex flex-col justify-between text-center text-white max-w-[700px]">
-                    <h2 className="text-2xl md:text-3xl text-b_col3 font-semibold mb-2">
+                    <h2 className="text-[5vw] text-b_col3 font-semibold mb-2">
                       {slide.title}
                     </h2>
-                    <p className="text-sm md:text-base">
+                    <p className="text-[2.5vw]">
                       {slide.description}
                     </p>
                   </div>
@@ -72,7 +71,7 @@ const Project = () => {
                     />
                     <div className="rounded-[36px] p-0.5 bg-gradient-to-b flex justify-center from-b_col3 to-b_col4 h-auto lg:w-[500px] md:w-[400px] shadow-2xl shadow-[#7ac4ec]/30">
                       <div className="rounded-[calc(36px-1px)] p-6 md:p-12 md:h-auto bg-gradient-to-b md:w-full from-[#061b24] via-[#072031] to-[#000b11] flex flex-col text-center text-white max-w-[559px]">
-                        <h2 className="text-[3vh] md:text-[4vh] lg:text-[5vh] xl:text-[6vh] text-b_col3 font-semibold mb-4">
+                        <h2 className="text-[1vw] md:text-[3vw] lg:text-[2vw] xl:text-[2.25vw] text-b_col3 font-semibold mb-4">
                           {slide.title}
                         </h2>
                         <p className="text-[1.5vh] md:text-[1.6vh] lg:text-[1.75vh] xl:text-[2vh]">
