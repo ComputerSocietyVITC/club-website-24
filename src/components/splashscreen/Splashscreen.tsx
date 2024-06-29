05817ac8d269a7dff3d91f96e0eef386d88d8516
sam-shervin <samshervin.sebastian@gmail.com>
2024-06-29 18:45:40 +0530

    Splashscreen
    Co-authored-by: Aayush Shukla <me@a2ys.dev>

---
+++ src/components/splashscreen/Splashscreen.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Splashscreen = () => {
+    const [showSplash, setShowSplash] = useState(true);
+    const onAnimationComplete = () => {
+        setShowSplash(false);
+    };
+    return (
+        <>
+            {showSplash && (
+                <motion.div
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 0 }}
+                    transition={{ duration: 1, delay: 1 }}
+                    className="fixed flex bg-gradient-to-b from-[#000b11] from-2% via-[#072031] to-[#000b11] to-9% h-screen w-screen justify-center z-[100]"
+                    onAnimationComplete={onAnimationComplete}
+                >
+                    <motion.img
+                        src="/assets/comsocLogowhite.webp"
+                        alt="Icon"
+                        className="self-center scale-50"
+                    />
+                </motion.div>
+            )}
+        </>
+    );
+};
+
+export default Splashscreen;
---
+++ src/ts/Blog.tsx
@@ -0,0 +1,35 @@
+import BlogCard from "../components/BlogCard";
+import Footer from "../components/footer";
+import { getSortedBlogDataInDescendingOrder } from "../data/blogdata";
+
+const Blogs = () => {
+  const sortedBlogData = getSortedBlogDataInDescendingOrder();
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center scale-80 pt-32 pb-16 bg-transparent">
+        <div className="text-white text-7xl font-bold text-center pb-16">
+          Blogs
+        </div>
+
+        <div className="w-full max-w-6xl mb-16">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
+            {sortedBlogData.map((blog, index) => (
+              <BlogCard
+                key={index}
+                title={blog.title}
+                date={blog.date}
+                author={blog.author}
+                imageUrl={blog.imageUrl}
+                url={blog.url}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Blogs;
